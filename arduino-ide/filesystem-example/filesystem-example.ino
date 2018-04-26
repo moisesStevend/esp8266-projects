62aaf93797dcef2ad85d8ec2077498cc0371df1a
Moises Stevend Meza Rodriguez <moises.stevend@gmail.com>
2018-04-26 17:06:35 -0500

    spiffs para wifi

--- arduino-ide/filesystem-example/filesystem-example.ino
+++ arduino-ide/filesystem-example/filesystem-example.ino
@@ -3,32 +3,23 @@
 void setup() {
   Serial.begin(115200);
 
-  // always use this to "mount" the filesystem
   bool result = SPIFFS.begin();
   Serial.println("SPIFFS opened: " + result);
-
-  // this opens the file "f.txt" in read-mode
-  File f = SPIFFS.open("/f.txt", "r");
+  File f = SPIFFS.open("/database.txt", "r");
   
   if (!f) {
     Serial.println("File doesn't exist yet. Creating it");
-
-    // open the file in write mode
-    File f = SPIFFS.open("/f.txt", "w");
+    File f = SPIFFS.open("/database.txt", "w");
     if (!f) {
       Serial.println("file creation failed");
     }
-    // now write two lines in key/value style with  end-of-line characters
-    f.println("ssid=abc");
-    f.println("password=123455secret");
+    f.println("ssid=jicamarca");
+    f.println("password=radioobsevatory");
   } else {
-    // we could open the file
     while(f.available()) {
-      //Lets read line by line from the file
       String line = f.readStringUntil('\n');
       Serial.println(line);
     }
-
   }
   file.close();
 }
